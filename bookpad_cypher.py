733b54a18ad4e96cca2bebd3eaa7a2c466466d58
flamango-juice <ethanalburo0221@gmail.com>
2025-04-07 17:21:11 -0700

    HAHAHAHAHAHA i still struggled despite copypasting

---
+++ bookpad_cypher.py
@@ -0,0 +1,160 @@
+import json
+import os
+import random
+import re
+import sys
+
+HOW_MANY_BOOK = 4 # books to use
+LINE = 128
+PAGE = 64
+pages = {}
+page_number= 0
+line_window = {}
+line_number = 0
+char_window = []
+
+def clean_line(line):
+    return line.strip().replace( "-", "") + " " # adding a space instead of new line
+
+def process_books(*paths):
+    for path in paths:
+        read_book(path)
+
+def read_book(file_name):
+    global char_window
+    with open(file_name, 'r', encoding='utf-8-sig') as fp:
+        for line in fp:
+            line = clean_line(line)
+            if line.strip():
+                for c in line:
+                    process_char(c)
+    if len(char_window) > 0:
+        add_line()
+    if len(line_window) > 0:
+        add_page()
+
+def process_char(char):
+    global char_window
+    char_window.append(char)
+    if len(char_window) == LINE:
+        add_line()
+
+
+def add_line():
+    global char_window, line_number
+    line_number += 1
+    process_page( ''.join(char_window), line_number )
+    char_window.clear()
+
+
+def process_page(line, line_num):
+    global line_window, pages, page_number
+    line_window[line_num] = line
+    if len(line_window) == PAGE:
+        add_page()
+
+
+def add_page():
+    global line_number, line_window, pages, page_number
+    page_number += 1
+    pages[page_number] = dict(line_window)
+    line_window.clear()
+    line_number = 0
+
+def generate_code_book():
+    global pages
+    code_book = {}
+    for page, lines in pages.items():
+        for num, line in lines.items():
+            for pos, char in enumerate(line):
+                code_book.setdefault(char, []).append(f'{page}-{num}-{pos}')
+    return code_book
+
+
+def save(file_path, book):
+    with open(file_path, 'w') as fp:
+        # json.dump(book, fp, indent=4)
+        json.dump(book, fp)
+
+
+def load(file_path, *key_books, reverse=False):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as fp:
+            return json.load(fp)
+    else:
+        process_books(*key_books)
+        if reverse:
+            save(file_path, pages)
+            return pages
+        else:
+            code_book = generate_code_book()
+            save(file_path, code_book)
+            return code_book
+
+def encrypt(code_book, message):
+    cipher_text = []
+    for char in message:
+        index = random.randint(0, len(code_book[char]) - 1)
+        cipher_text.append(code_book[char].pop(index))
+    return '-'.join(cipher_text)
+
+def decrypt(rev_code_book, ciphertext):
+    plaintext = []
+    for cc in re.findall(r'\d+-\d+-\d+', ciphertext):
+        page, line, char = cc.split('-')
+        plaintext.append(
+            rev_code_book[page][line][int(char)])
+    return ''.join(plaintext)
+
+
+def main_menu():
+    print("""1). Encrypt
+2). Decrypt
+3). Quit
+""")
+    return int(input("Make a selection [1,2,3]: "))
+
+
+def main():
+    key_books = ('Books/Jose Rizal - Ang Filibusterismo (1891, tl).txt', 'Books/Jose Rizal - Noli Me Tangere (1887, tl).txt', 'Books/Vincente Rama - Larawan - Mga Sugilanon ug Dinalidali.txt', "Books/Yoon Suk Yeol wikipedia article")
+    code_book_path = 'code_books/standard.json'
+    rev_code_book_path = 'code_books/reverse.json'
+    while True:
+        try:
+            choice = main_menu()
+            match (choice):
+                case 1:
+                    code_book = load(code_book_path, *key_books)
+                    message = input("Please enter your secret message: ")
+                    print(encrypt(code_book, message))
+                    continue
+                case 2:
+                    rev_code_book = load(rev_code_book_path, *key_books, reverse=True)
+                    message = input("Please enter your cipher text: ")
+                    print(decrypt(rev_code_book, message))
+                    continue
+                case 3:
+                    sys.exit(0)
+        except ValueError as ve:
+            print("Improper selection.")
+if __name__ == "__main__":
+    main()
+    """
+     book1 = "Books/Jose Rizal - Ang Filibusterismo (1891, tl).txt"
+    book2 = "Books/Jose Rizal - Noli Me Tangere (1887, tl).txt"
+    book3 = "Books/Yoon Suk Yeol wikipedia article"
+    book5 = "Books/Vincente Rama - Larawan - Mga Sugilanon ug Dinalidali.txt"
+    rev_codebook = "reverse.json"
+    process_books(book1,book2,book3,book5)
+    cb = generate_code_book()
+    ciphertext = encrypt(cb,"TRADE WARS SUCK!!!")
+    print(ciphertext)
+    rcode = load(rev_codebook,book1,book2,book3,book5,reverse=True)
+    message_decrypt = decrypt(
+        rcode,
+        ciphertext
+    )
+    print(message_decrypt)  
+    
+    """
+
